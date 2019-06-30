4b3644fbf9b7b7bf564228c14e38589c88d6e225
Rana Faizan Ur Rahman Khan <faizi_733@ucp.edu.pk>
2019-06-30 18:31:52 +0500

    updated some work

---
+++ admin/insert_brand.php
@@ -0,0 +1,91 @@
+<?php
+$con = mysqli_connect("localhost","root","","huungry");
+if(!$con)
+{
+    echo "Not Connected";
+}
+if(isset($_POST['insert_brand']))
+{
+    $title =  $_POST['brand_title'];
+    print_r($_POST);
+
+    $q = "insert into brands (brand_title)
+            values('$title')";
+    mysqli_query($con,$q);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hunngry</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+    <meta name="description" content="Food Delivey">
+    <meta name="keywords" content="pizza,delivery,food,hungry,huungry,eat,home delivery, food near me">
+    <meta name="author" content="Rana Faizan Ur Rahman Khan, Hamza Rehman">
+
+    <link rel="stylesheet" href="../css\style.css">
+    <link rel="stylesheet" href="css\navbar.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+<body class="col col-12">
+<header>
+
+</header>
+<!--<hr>-->
+<main class="homeindex">
+    <div class="fullcontainer">
+        <div class="container col-10">
+            <form action="" method="post" enctype="multipart/form-data">
+
+                <div class="row">
+                    <div class="col-12">
+                        <h1>Add New Brands</h1>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col col-4">
+                        <label for="cat_title"><span>Title:</span></label>
+                    </div>
+                    <div class="col col-8">
+                        <div>
+                            <input type="text" class="form-control" id="brand_title" name="brand_title"
+                                   placeholder="Enter Brand Title" required pattern="([A-Z]|[a-z]){2,}">
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col col-12"></div>
+                        <div class="col col-12">
+                            <button type="submit" name="insert_brand" class="submitBTTN"><i class="fas fa-plus"></i>
+                                Insert Now
+                            </button>
+                        </div>
+                    </div>
+
+            </form>
+        </div>
+    </div>
+</main>
+
+<script src="../js/jquery-3.3.1.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+
+    });
+</script>
+
+</body>
+</html>
