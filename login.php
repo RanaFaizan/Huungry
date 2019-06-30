3632a999e957832789d2dfcd0b76cf226a9bc29c
Rana Faizan Ur Rahman Khan <faizi_733@ucp.edu.pk>
2019-06-30 19:34:32 +0500

    updated some work

--- login.php
+++ login.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+require_once 'db_connection.php';
+$error_msg = '';
+if(isset($_POST['login'])){
+    $email = $_POST['user_email'];
+    $pass = $_POST['user_pass'];
+    $sel_user = "select * from customers where email='$email' AND cus_pass='$pass'";
+    $run_user = mysqli_query($con, $sel_user);
+    $check_user = mysqli_num_rows($run_user);
+    if($check_user==0){
+        $error_msg = 'Password or Email is wrong, try again';
+    }
+    else{
+        $_SESSION['user_email'] = $email;
+        if(!empty($_POST['remember'])) {
+            setcookie('user_email', $email, time() + (10 * 365 * 24 * 60 * 60));
+            setcookie('user_pass', $pass, time() + (10 * 365 * 24 * 60 * 60));
+        } else {
+            setcookie('user_email','' );
+            setcookie('user_pass', '');
+        }
+        header('location:index.php?logged_in=You have successfully logged in!');
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,7 +52,7 @@
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
             <li><a href="forget-password.php">Forget-Password</a></li>
-            <li><div class="search_bar"><input type="text" name="search" placeholder="Search.."></div></li>
+            <li><div class="search_bar"><input class="search_b" type="text" name="search" placeholder="Search.."></div></li>
         </ul>
     </nav>
 </header>
@@ -34,40 +60,59 @@
 <main class="cform">
     <div class="fullcontainer">
         <div class="container col-10">
-            <form action="/action_page.php">
+            <form action="login.php" method="post"">
 
-                <div class="row">
-                    <div class="col-12">
-                        <h1>Login</h1>
+            <div class="row">
+                <div class="col-12">
+                    <h1>User Login</h1>
+                    <div class="col col-12" style="padding-left: 35%;padding-bottom: 1%; color: #b21f2d">
+                        <b>
+                            <h3>
+                                <?php echo $error_msg;?>
+                            </h3>
+                        </b>
                     </div>
                 </div>
+            </div>
 
-                <div class="row">
+            <div class="col col-12" style="padding-left: 35%;padding-bottom: 1%; color: #b21f2d">
+                <b>
+                    <h3 class="text-danger"><?php echo @$_GET['not_admin']?></h3>
+                    <h3 class="text-primary"><?php echo @$_GET['logged_out']?></h3>
+                </b>
+            </div>
 
-                    <div class="col col-3"><label for="email"><span>Email</span></label></div>
-                    <div class="col col-7"><input type="text" id="email" name="email" placeholder="Your email address.." required pattern="[a-z]+(\w|\.|)?(([a-z]|[0-9])*)?@(([a-z]{3,7}\.com)|([a-z]{3,5}\.[^com]{3,5}\.pk))"></div>
+            <div class="row">
 
-                    <div class="col col-3"><label for="pass"><span>Password</span></label></div>
-                    <div class="col col-7"><input type="password" id="pass" name="pass" placeholder="Your passwordr.." required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$"></div>
+                <div class="col col-3"><label for="email"><span>Email</span></label></div>
+                <div class="col col-7"><input type="text" id="user_email" name="user_email" value="<?php echo @$_COOKIE['user_email']?>" placeholder="Your email address.." required pattern="[a-z]+(\w|\.|)?(([a-z]|[0-9])*)?@(([a-z]{3,7}\.com)|([a-z]{3,5}\.[^com]{3,5}\.pk))"></div>
 
+                <div class="col col-3"><label for="pass"><span>Password</span></label></div>
+                <div class="col col-7"><input type="password" id="user_pass" name="user_pass" value="<?php echo @$_COOKIE['user_pass']?>" placeholder="Your passwordr.." required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$"></div>
+
+            </div>
+
+            <div class="col col-12" style="padding-left: 34%;padding-bottom: 2%">
+                <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                <label class="form-check-label" for="remember" style="color: #721c24"><b>Remember me</b></label>
+            </div>
+
+            <div class="row">
+                <div class="col col-2">
+                    <input class="submitBTTN" type="submit" name="login" value="Sign In">
                 </div>
 
-                <div class="row">
-                    <div class="col col-2">
-                        <input class="submitBTTN" type="submit" value="Login">
-                    </div>
-
-                    <div class="col col-2">
-                        <input class="submitBTTN" type="button" value="Forgot Password" onclick="window.location.href='forget-password.html'">
-                    </div>
+                <div class="col col-2">
+                    <input class="submitBTTN" type="button" value="Forgot Password" onclick="window.location.href='forget-password.html'">
                 </div>
+            </div>
 
 
-                <div class="row">
-                    <div class="col col-12" style="padding-top: 3%;">
-                        <input style="width: 100%; font-size: large;" type="submit" value="Go to Admin Login" onclick="window.location.href='admin/login.php'">
-                    </div>
+            <div class="row">
+                <div class="col col-12" style="padding-top: 3%;">
+                    <input style="width: 100%; font-size: large;" type="submit" value="Go to Admin Login" onclick="window.location.href='admin/login.php'">
                 </div>
+            </div>
 
             </form>
         </div>
@@ -125,5 +170,7 @@
     </div>
 
 </footer>
+<script src="../js/jquery-3.3.1.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
 </body>
 </html>
