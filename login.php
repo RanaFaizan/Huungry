20ecde93f70fb70d83b48d918d36b7488043dae0
Rana Faizan Ur Rahman Khan <faizi_733@ucp.edu.pk>
2019-06-30 17:33:59 +0500

    Resolved some errors

--- login.php
+++ login.php
@@ -9,9 +9,6 @@
     <meta name="author" content="Rana Faizan Ur Rahman Khan, Hamza Rehman">
 
     <link rel="stylesheet" href="css\style.css">
-    <link rel="stylesheet" href="css\form.css">
-    <link rel="stylesheet" href="css\coloumns.css">
-    <link rel="stylesheet" href="css\responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
@@ -20,15 +17,15 @@
 <header>
     <nav>
         <div>
-            <a href="index.html"><img src="images/logo1.png" alt="Home"></a>
+            <a href="index.php"><img src="images/logo1.png" alt="Home"></a>
         </div>
 
         <ul>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="register.html">Register</a></li>
-            <li><a href="forget-password.html">Forget-Password</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="forget-password.php">Forget-Password</a></li>
             <li><div class="search_bar"><input type="text" name="search" placeholder="Search.."></div></li>
         </ul>
     </nav>
@@ -65,6 +62,13 @@
                     </div>
                 </div>
 
+
+                <div class="row">
+                    <div class="col col-12" style="padding-top: 3%;">
+                        <input style="width: 100%; font-size: large;" type="submit" value="Go to Admin Login" onclick="window.location.href='admin/login.php'">
+                    </div>
+                </div>
+
             </form>
         </div>
     </div>
@@ -77,13 +81,13 @@
         <div class="hide col-3 column">
 
             <ul>
-                <a href="index.html"><img class="logo" src="images/logo.png" alt="Home"></a>
+                <a href="index.php"><img class="logo" src="images/logo.png" alt="Home"></a>
             </ul>
 
         </div>
 
         <div class="col-3 column">
-            <a href="about.html"><h2>About Us</h2></a>
+            <a href="about.php"><h2>About Us</h2></a>
             <div>
                 <address>
                     Developed By<br>
@@ -96,7 +100,7 @@
 
         <div class="col-3 column">
 
-            <a  href="contact.html"><h2>Contact Us</h2></a>
+            <a  href="contact.php"><h2>Contact Us</h2></a>
             <div>
                 <address>
                     Email: faizi_733@ucp.edu.pk<br>
@@ -108,12 +112,12 @@
 
         <div class="col-3 column">
 
-            <a href="about.html"><h2>Stay Connected</h2></a>
+            <a href="about.php"><h2>Stay Connected</h2></a>
             <div class="icons">
-                <a class="icon1" href="about.html"><i class="fab fa-facebook-square"></i></a>
-                <a class="icon2" href="about.html"><i class="fab fa-twitter"></i></a>
-                <a class="icon3" href="about.html"><i class="fab fa-youtube"></i></a>
-                <a class="icon4" href="about.html"><i class="fab fa-instagram"></i></a>
+                <a class="icon1" href="about.php"><i class="fab fa-facebook-square"></i></a>
+                <a class="icon2" href="about.php"><i class="fab fa-twitter"></i></a>
+                <a class="icon3" href="about.php"><i class="fab fa-youtube"></i></a>
+                <a class="icon4" href="about.php"><i class="fab fa-instagram"></i></a>
             </div>
 
         </div>
