9c47ee29a8e5fb1891af19e235c9c9dff3515790
Rana Faizan Ur Rahman Khan <faizi_733@ucp.edu.pk>
2019-07-09 15:16:18 +0500

    updated files

---
+++ categories.php
@@ -0,0 +1,125 @@
+<?php
+if(!isset($_SESSION['user_email'])){
+    header('location: login.php?not_admin=You are not Admin!');
+}
+require_once "db_connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hunngry</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type">
+    <meta name="description" content="Food Delivey">
+    <meta name="keywords" content="pizza,delivery,food,hungry,huungry,eat,home delivery, food near me">
+    <meta name="author" content="Rana Faizan Ur Rahman Khan, Hamza Rehman">
+
+    <link rel="stylesheet" href="css\style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+<body class="col col-12">
+<header>
+
+</header>
+<!--<hr>-->
+<main class="homeindex">
+    <div class="fullcontainer">
+        <div class="container col-10" style="margin-top: 25%;">
+            <form action="/action_page.php">
+
+                <div class="row">
+                    <div class="col-12">
+                        <h1>Our Categories</h1>
+                    </div>
+                </div>
+
+                <div class="row" style="font-size: larger; margin-bottom: 5%">
+
+                    <div class="col col-1"><label for="fname"><span style="color: #721c24"><b>ID</b></span></label></div>
+                    <div class="col col-2"><label for="fname"><span style="color: #721c24"><b>TITLE</b></span></label></div>
+
+                </div>
+
+
+                <?php
+                $catQuery = "select * from categories";
+                $catQueryResult = mysqli_query($con,$catQuery);
+
+                while($row = mysqli_fetch_assoc($catQueryResult))
+                {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+                    echo "<div class=\"row\">
+                            <div class=\"col col-1\"><label for=\"fname\"><span>$cat_id</span></label></div>
+                            <div class=\"col col-2\"><label for=\"fname\"><span>$cat_title</span></label></div>
+                        </div>";
+                }
+                ?>
+            </form>
+        </div>
+    </div>
+</main>
+<!--<hr>-->
+<footer>
+
+    <div class="row">
+
+        <div class="hide col-3 column">
+
+            <ul>
+                <a href="index.php"><img class="logo" src="images/logo.png" alt="Home"></a>
+            </ul>
+
+        </div>
+
+        <div class="col-3 column">
+            <a href="about.php"><h2>About Us</h2></a>
+            <div>
+                <address>
+                    Developed By<br>
+                    Rana Faizan Ur Rahman Khan,<br>
+                    Hamza Rehman<br>
+                </address>
+            </div>
+
+        </div>
+
+        <div class="col-3 column">
+
+            <a  href="contact.php"><h2>Contact Us</h2></a>
+            <div>
+                <address>
+                    Email: faizi_733@ucp.edu.pk<br>
+                    Phone: 090078601
+                </address>
+            </div>
+
+        </div>
+
+        <div class="col-3 column">
+
+            <a href="about.php"><h2>Stay Connected</h2></a>
+            <div class="icons">
+                <a class="icon1" href="about.php"><i class="fab fa-facebook-square"></i></a>
+                <a class="icon2" href="about.php"><i class="fab fa-twitter"></i></a>
+                <a class="icon3" href="about.php"><i class="fab fa-youtube"></i></a>
+                <a class="icon4" href="about.php"><i class="fab fa-instagram"></i></a>
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+
+<script src="js/slider.js"></script>
+<script src="../js/jquery-3.3.1.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+
+</body>
+</html>
